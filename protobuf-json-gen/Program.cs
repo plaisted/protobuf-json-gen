696163cb228cea99c003206cdaed86f962914bb1
plaisted <git@plaisted.io>
2017-11-04 22:58:15 -0500

    Add project files.

---
+++ protobuf-json-gen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace protobuf_json_gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
